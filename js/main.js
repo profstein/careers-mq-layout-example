83d273909c7a24eb3f813ecc3ba99dc23385cdf0
Christopher Stein <prof.stein@gmail.com>
2014-12-09 13:12:49 -0500

    making html consistent and getting javascript ready
    Adding schedule.html and sessions.html and javascript shell and making
    the html files consistent with same header, footer and nav

---
+++ js/main.js
@@ -0,0 +1,26 @@
+/* we are putting it in a self invoking anonymous function to shield our variables from other scripts */
+(function(){
+	
+	/* ===================================
+	  Document Ready
+	  ==================================
+	*/
+	jQuery(document).ready(function($) { console.log("ready");
+		// Stuff to do as soon as the DOM is ready. 
+        // You can use $() w/o colliding with other libs
+                                            
+        console.log("document is ready");                                
+        // ---- Paste Document.ready code here
+        
+                                        
+	});/* ------------------- end document ready ------------------- */
+
+
+	/* ===================================
+	  Functions
+	  ==================================== */
+
+
+
+
+})();//End self invoking anonymous function
